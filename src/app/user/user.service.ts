afd09bbd06e2625a7a0dcc7ff08a31cadf525e9d
Hoang <hoangkai25@gmail.com>
2023-06-15 10:51:29 +0700

    Revert "a"
    This reverts commit 7a647862ec378f4b94cb5757f9cd0f00d70f8e5b.

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -114,6 +114,7 @@ export class UserService {
       phone,
       address,
       gender,
+      
     }; // Body của request
 
     return this.http.post(url, body); // Gửi request và trả về response dưới dạng Observable
